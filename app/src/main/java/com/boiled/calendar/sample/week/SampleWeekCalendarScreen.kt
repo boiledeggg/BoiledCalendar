61b8cba01d4723ecb21490df476b7d2951327167
boiledegg <tjrwns2163@naver.com>
2025-02-19 01:23:46 +0900

    [ui] sample screen enhanced

--- app/src/main/java/com/boiled/calendar/sample/week/SampleWeekCalendarScreen.kt
+++ app/src/main/java/com/boiled/calendar/sample/week/SampleWeekCalendarScreen.kt
@@ -3,7 +3,6 @@ package com.boiled.calendar.sample.week
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,8 +12,6 @@ import androidx.compose.ui.unit.dp
 @Composable
 internal fun SampleWeekCalendarScreen(
     modifier: Modifier = Modifier,
-    onMonthNavigate: () -> Unit = {},
-    onWeekNavigate: () -> Unit = {},
 ) {
     Column(
         modifier = modifier.fillMaxSize(),
