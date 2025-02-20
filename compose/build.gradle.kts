b38bb4d539e9586c80e832a16c8af365e46d0a9f
boiledegg <tjrwns2163@naver.com>
2025-02-20 18:12:17 +0900

    [add] maven dependencies added

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,14 +1,53 @@
 import com.boiled.calendar.buildlogic.dsl.setNameSpace
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
     id("com.boiled.calendar.buildlogic.convention.compose")
     id("com.boiled.calendar.buildlogic.primitive.test")
+    alias(libs.plugins.maven)
 }
 
 android {
     setNameSpace("calendar.compose")
 }
 
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    signAllPublications()
+
+    coordinates("io.github.boiledeggg", "boiled-calendar", libs.versions.versionName.get())
+
+    pom {
+        name.set("Boiled Calendar")
+        description.set("Compose Calendar")
+        url.set("https://github.com/boiledeggg/BiledCalendar")
+        inceptionYear.set("2025")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/boiledeggg/BoiledCalendar/blob/main/LICENSE")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("LeeSeokJun")
+                name.set("BoiledEgg")
+                email.set("https://github.com/boiledeggg")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/boiledeggg/BoiledCalendar")
+            connection.set("scm:git:github.com/boiledeggg/BoiledCalendar.git")
+            developerConnection.set("scm:git:ssh://github.com:boiledeggg/BoiledCalendar.git")
+        }
+    }
+}
+
+
 dependencies {
-    implementation(project(":core"))
+    api(project(":core"))
 }
